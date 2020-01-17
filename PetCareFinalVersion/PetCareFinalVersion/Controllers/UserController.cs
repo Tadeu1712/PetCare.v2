f4b43bc28b334b99be2628c6ad376a814eb5f804
Rutraz <rutraavs@gmail.com>
2020-01-17 03:26:12 +0000

    Login/Auth/Token

--- PetCareFinalVersion/PetCareFinalVersion/Controllers/UserController.cs
+++ PetCareFinalVersion/PetCareFinalVersion/Controllers/UserController.cs
@@ -3,6 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 using PetCareFinalVersion.Models;
 using System.Text.Json;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using PetCareFinalVersion.Data;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using BCrypt;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.Extensions.Configuration;
 
 namespace PetCareFinalVersion.Controllers
 {
@@ -11,52 +22,139 @@ namespace PetCareFinalVersion.Controllers
     public class UserController : Controller
     {
         private readonly AppDbContext _context;
+        private IConfiguration _config;
 
-        public UserController(AppDbContext context)
+        // UserController Constructor
+        public UserController(AppDbContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
 
+        //Api que devolve todos os users em formato JSON
+        [Produces("application/json")]
         [HttpGet("all")]
-        public string allUsers()
+        public async Task<IActionResult> AllUsers()
         {
-            var json = _context.Users.ToList();
-            if (json == null){
-               
+            try
+            {
+                var json = _context.Users.ToList();
+                if (json == null)
+                {
+
+                }
+                return Ok(json);
             }
-            return JsonSerializer.Serialize(json);
+            catch
+            {
+                return BadRequest();
+            }
+
         }
 
-        [HttpGet("{id}")]
-        public string allUsers(int id)
+        [Produces("application/json")]
+        [HttpGet("find/{id}")]
+        public async Task<IActionResult> User(int id)
         {
-            var json = _context.Users.Find(id);
-
-            if(json == null)
+            try
             {
-                
-            }
+                User query = _context.Users.Find(id);
 
-            return JsonSerializer.Serialize(json);
+                return Ok(query);
+            }
+            catch
+            {
+                return BadRequest();
+            }
         }
 
         [Produces("application/json")]
         [Consumes("application/json")]
         [HttpPost("create")]
-        public User CreateUser([FromBody] User obj)
+        public async Task<IActionResult> Create([FromBody]User aUser)
         {
-              var queryUser = new User()
+            try
             {
-                Name = obj.Name,
-                Email = obj.Email,
-                Password = obj.Password,
-                Admin = true,
-            };
-            
-            _context.Users.Add(queryUser);
-            _context.SaveChanges();
-            return queryUser;
+
+                var queryUser = new User()
+                {
+                    Name = aUser.Name,
+                    Email = aUser.Email,
+                    Password = BCrypt.Net.BCrypt.EnhancedHashPassword(aUser.Password),
+                    Admin = false,
+                };
+
+                _context.Users.Add(queryUser);
+                _context.SaveChanges();
+
+                return Ok(queryUser);
+            }
+            catch
+            {
+                return BadRequest();
+            }
         }
 
+        [Produces("application/json")]
+        [Consumes("application/json")]
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            try
+            {
+                var user = _context.Users.Find(id);
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+                return Ok("Deleted " + id);
+            }
+            catch
+            {
+                return NotFound("User " + id + " not found!");
+            }
+        }
+
+        [Produces("application/json")]
+        [Consumes("application/json")]
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginUser(Login aLogin)
+        {
+            try
+            {
+                IActionResult response = Unauthorized();
+                var user = Auth.Login(aLogin.email, aLogin.pass, _context);
+                if (user != null)
+                {
+                    var tokenString = GenerateJSONWebToken(aLogin);
+                    user.Token = tokenString;
+                    _context.SaveChanges();
+                    response = Ok(user);
+                    return response;
+                }
+                else
+                {
+                    return BadRequest("Wrong Password");
+                }
+
+            }
+            catch
+            {
+                return BadRequest("Wrong email");
+            }
+        }
+
+        private string GenerateJSONWebToken(Login aLogin)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              null,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
